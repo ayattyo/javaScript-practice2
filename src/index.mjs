67cf854d8c9d043c832d6e00168a9b1b3d907f85
ayattyo <sukiyaki630@icloud.com>
2024-08-13 13:00:33 +0000

    init

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,11 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+const num = 1 + 1;
+console.log(num);
+const bookTitle = "JavaScript Primer";
+console.log(bookTitle);
+console.log(1);
+const total = 42 + 42;
+console.log(total);
+function double(num) {
+  return num * 2;
+}
+console.log(double(1000));
